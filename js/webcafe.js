8842eafb6b9389b17bf49ec48f8b1d63b3a87d39
JEONGSUJI <wtw9611@gmail.com>
2019-09-11 19:42:06 +0900

    webcafe 메뉴 js 적용 및 login part까지 복습

--- js/webcafe.js
+++ js/webcafe.js
@@ -11,3 +11,13 @@ tab.on('click', function(e){
 // e 는 발생한 event를 기억하고 있을 것임
 // preventDefault는 기본 이벤트를 지움
 //  $(this).parent().parent(); 메소드 체인이라고 부름 
+
+
+var menu = $('.menu li');
+var tab_menu = $('.btn-menu');
+
+tab_menu.on('click', function(e){
+  e.preventDefault();
+  menu.removeClass('menu-act');
+  $(this).parent().addClass('menu-act');
+})
